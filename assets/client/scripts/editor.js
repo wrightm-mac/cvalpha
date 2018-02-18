2fa833c16ad6ea9cb0f32aa02bb8763f04b9ad91
Michael <wrightm@mac.com>
2018-02-18 21:48:07 +0000

    Add new rows to sections.

--- assets/client/scripts/editor.js
+++ assets/client/scripts/editor.js
@@ -46,7 +46,40 @@ $(function() {
     }
   }
 
+  function passClick() {
+    let $span = $("span.editorClickable", this).first();
+    if ($span) {
+      startEdit.call($span, $span);
+    }
+
+    return false;
+  }
+
   $("body").click(endEdit);
 
   $("div#cvPersonal .editorClickable").click(startEdit);
+
+  $("td").click(passClick);
+
+  $("span.editorAdd").click(function() {
+    let $parent = $(this).parent();
+    let $table = $("table", $parent).first();
+    let columns = Number.parseInt($table.attr("data-columns"));
+    console.log("add-record (columns=%d)", columns);
+
+    let $row = $("tr", $table).last();
+    let $new = $row
+                .clone()
+                .attr("data-id", "");
+
+    $("td", $new).click(passClick);
+    $("input", $new).attr("checked", true);
+    $("span", $new)
+      .addClass("editorClickable")
+      .addClass("editorModified")
+      .text("")
+      .click(startEdit);
+
+    $table.append($new);
+  });
 });

6932218d76eeaeb53e4e319ef886d6342c8707ed
Michael <wrightm@mac.com>
2018-02-19 10:02:16 +0000

    Add row delete.

--- assets/client/scripts/editor.js
+++ assets/client/scripts/editor.js
@@ -1,3 +1,37 @@
+/* ----------------------------------------------------------------------------
+
+                            BSD 3-Clause License
+
+                        Copyright (c) 2018, wrightm-mac
+                            All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+  * Neither the name of the copyright holder nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+----------------------------------------------------------------------------- */
+
 $(function() {
   var $text;
   var $edit;
@@ -55,22 +89,22 @@ $(function() {
     return false;
   }
 
+  function deleteRow() {
+    let $row = $(this).parent();
+    $row.remove();
+  }
+
   $("body").click(endEdit);
 
   $("div#cvPersonal .editorClickable").click(startEdit);
 
   $("td").click(passClick);
 
-  $("span.editorAdd").click(function() {
-    let $parent = $(this).parent();
-    let $table = $("table", $parent).first();
-    let columns = Number.parseInt($table.attr("data-columns"));
-    console.log("add-record (columns=%d)", columns);
+  $(".editorAdd").click(function() {
+    let $table = $(this).parent().parent();
 
     let $row = $("tr", $table).last();
-    let $new = $row
-                .clone()
-                .attr("data-id", "");
+    let $new = $row.clone().attr("data-id", "");
 
     $("td", $new).click(passClick);
     $("input", $new).attr("checked", true);
@@ -80,6 +114,10 @@ $(function() {
       .text("")
       .click(startEdit);
 
+    $(".editorDelete", $new).click(deleteRow);
+
     $table.append($new);
   });
+
+  $(".editorDelete").click(deleteRow);
 });
